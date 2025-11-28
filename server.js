e936b1375bef99b2a99e0df773252598aeab94c3
HenaQuamar <quamarhena88@gmail.com>
2025-11-28 14:47:17 +0530

    i just added api in this app

--- server.js
+++ server.js
@@ -12,9 +12,41 @@ app.use("/users",userRoutes);
 app.use("/uploads", express.static("uploads"));
 app.use("/address",addressRoutes);
 
+const db=require("./db");
+app.post("/locationupdate", (req, res) => {
+  const {deliveryBoyId, orderId, latitude,longitude, speed} = req.body;
+
+  if (!deliveryBoyId || !orderId || !latitude || !longitude || !speed) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
+
+  const sql = `
+    INSERT INTO delivery_locations 
+    (deliveryBoyId, orderId, latitude, longitude, speed) 
+    VALUES (?, ?, ?, ?, ?);
+  `;
+
+  db.query(
+    sql,
+    [deliveryBoyId, orderId, latitude, longitude, speed],
+    (err,result) => {
+      if (err) {
+        return res.status(500).json({ message: "DB error", error: err });
+      }
+      res.json({ message: "Location track successfully",data:result });
+    }
+  );
+});
+
+
+
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,(err)=>{
     if(err) return console.error("server error :"+err.message);
     console.log(`server running on port ${PORT}`);
 });
+
+
+
+
 
