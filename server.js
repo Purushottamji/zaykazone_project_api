20d673d02ee96770916b3928adafbf91af796a78
Purushottam Kumar <purushottam2fx@gmail.com>
2025-12-03 10:15:05 +0530

    update express.static uploads

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoutes);
 app.use("/phone", phoneOtpRoutes);
 app.use("/users", userRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static("uploads"));
 app.use("/address", addressRoutes);
 app.use("/otp", otpRoutes);
 
