5d3040bd8ba2a7c76af7347d4180f7799689ce7b
Purushottam Kumar <purushottam2fx@gmail.com>
2025-12-09 14:22:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const mysql = require("mysql2/promise");
-// const dotenv = require("dotenv"); 
-// dotenv.config();
+const dotenv = require("dotenv"); 
+dotenv.config();
 
 const db = mysql.createPool({
-<<<<<<< HEAD
-    host: "localhost",//process.env.DB_HOST ||
-    user: "root",//process.env.DB_USER ||
-    password:"",//process.env.DB_PASS ||
-    database:"zayakazone",//process.env.DB_NAME ||
-    port: 3307
-=======
     host: process.env.DB_HOST ||"localhost",
     user: process.env.DB_USER ||"root",
     password:process.env.DB_PASS ||"",
     database:process.env.DB_NAME ||"zaykazone",
->>>>>>> 1eb770e54a53f8d864412642744f8cea87bf68ba
 });
 
 db.getConnection()
