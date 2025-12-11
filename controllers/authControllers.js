64965abd31b82c246a83b5b336527661835f30b4
your-github-username <your-email@example.com>
2025-12-11 12:28:49 +0530

    new work

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -71,4 +71,4 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, loginUser };
+module.exports = { registerUser, loginUser};
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const foodRoutes= require("./routes/foodRoutes");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const placeOrderRoutes=require("./routes/placeorderAddressRoutes");
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -27,6 +28,13 @@ app.use("/image_add", imageRoutes);
 app.use("/food", foodRoutes);
 app.use("/payment", paymentRoutes);
 app.use('/place',placeOrderRoutes);
+
+
+
+
+
+
+
 const db=require("./db");
 app.get("/getTablesWithColumns", async (req, res) => {
   try {
